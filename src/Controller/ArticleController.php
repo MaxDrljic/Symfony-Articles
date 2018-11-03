18dc15d674cd78f1f322dfde62b5253bd1289474
MaxDrljic <max_spree@hotmail.com>
2018-11-03 10:22:38 +0100

    Add articles through form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,17 @@
 
   use App\Entity\Article;
 
+  // Core Symfony imports
+  use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+  // Imports for Form
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
   class ArticleController extends Controller {
     # Instead of specifying routes in config/routes.yaml,
@@ -22,6 +28,43 @@
       $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
       return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+      $article = new Article();
+
+      $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, array('attr' =>
+        array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')
+        ))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Create',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+      $form->handleRequest($request);
+      
+      if($form->isSubmitted() && $form->isValid()) {
+        $article = $form->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('article_list');
+      }
+
+      return $this->render('articles/new.html.twig', array(
+        'form' => $form->createView()
+      ));
     }
 
     /**
@@ -49,4 +92,5 @@
 
       return new Response('Saved an article with the id of ' . $article->getId());
     } */
+    
   }
