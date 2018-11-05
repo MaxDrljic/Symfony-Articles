6888342f7b6d4150011f0bb187236210682ffb58
MaxDrljic <max_spree@hotmail.com>
2018-11-05 12:44:23 +0100

    Configure security.yaml & generated SecurityController

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -70,4 +71,16 @@ class User
         $this->email = $email;
         return $this;
     }
+
+    // UserInterface related functions...
+    public function getRoles()
+    {
+        return [
+            'ROLE_USER'
+        ];
+    }
+
+    public function getSalt() {}
+
+    public function eraseCredentials() {}    
 }
